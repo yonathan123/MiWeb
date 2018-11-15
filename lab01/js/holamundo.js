880e6ed9a4691ee0d02c6b90779fd226eeb3b037
yonathanmunayco <yonathan.munayco@vallegrande.edu.pe>
2018-11-15 17:15:53 -0500

    Agregando cambios en mis archivo

---
+++ lab01/js/holamundo.js
@@ -0,0 +1,20 @@
+// mi primer comentario
+console.log(`hola terricola ... te escribo desde el mas alla..`);
+
+// sumar los numeros 100 y 500
+ console.log( 100+500);
+
+ // comparar los siguientes numeros 800 y 500
+ console.log (800<500);
+
+ // imprime 3 nombres de paises
+ console.log(`peru , brasil, bolivia`);
+
+ //imprimir mensaje de error
+ console.error(`este error es de prueba`);
+
+ //multiplicar 3 numeros 10 x 25 x 30
+ console.log (10*25* 30);
+
+
+ 
---
+++ lab02/js/variables.js
@@ -0,0 +1,51 @@
+//definiendo variables
+var nombre = `jesus`;
+
+//imprimir contenido de la variable nombre
+console.log(nombre);
+
+//definiendo datos personales con variables
+var nompersona = `Gloria`;
+var apepersona = `munayco guzman`;
+var edadpersona = 20;
+var procpersona = `san vicente`;
+
+//imprimir datos personales
+console.log(`mi nombre es `+ nompersona + `y mis apellidos son ` + apepersona);
+console.log(`mi edad es ` + edadpersona + ` y soy del distrito de ` + procpersona);
+
+//definiendo variables numericas y calculos respectivos
+var numUno = 100;
+var numDos = 350;
+var suma = numUno + numDos;
+var resta = numDos - numUno;
+var producto = numUno* numDos;
+var divide = numUno / numDos;
+var mayor = numDos > numUno;
+var comparar = numUno == numDos;
+
+// imprimir resultados 
+console.log('la suma del ' + numUno + ' mas ' +  numDos +  ' es ' + suma);
+console.log ('la resta del ' -  numDos - ' menos ' - numUno - ' es '- resta);
+console.log(`la multiplicacion `* numUno * `por ` * numUno * `es` * producto); 
+
+//resolver
+var producto = 'zapatillas Nike';
+var cantidad = 10;
+var precio = 380;
+var total = cantidad * precio;
+//imprimir mensaje de resultado
+console.log ('el precio de las zapatillas en total' + producto + 'es' +total);
+
+//definir array con los dias de la semana
+ var diasSemana =   ['lunes', 'martes ', 'miercoles' , 'jueves', 'viernes '];
+ console.log(diasSemana);
+ //crear un array con 5 nombres de personas e imprimirlos en pantalla grupal e ind
+var nombrepersonas = ['Carlos' , 'Juan' , 'Naomi', 'Lisbeth', 'Antonio' ];
+console.log(nombrepersonas);
+console.log(nombrepersonas [0]);
+console.log(nombrepersonas[1]);
+console.log(nombrepersonas [2]);
+console.log(nombrepersonas[3]);
+console.log(nombrepersonas[4]);
+ 
